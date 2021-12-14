4d2377f2336468971b8242d69c5c685d590c1fc3
Artur <abezpalyi@gmail.com>
2021-12-14 10:41:00 +0200

    remove body scroll in open burger

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,21 @@ import swiper from './modules/swiper';
 // When DOM is ready
 $(() => {
   swiper.init();
+  const body = document.querySelector('body');
+  const btn = document.querySelector('.burger-btn');
+
+  // eslint-disable-next-line no-use-before-define
+  btn.addEventListener('click', () => scrollHandler());
+
+  const scrollHandler = () => {
+    // eslint-disable-next-line no-use-before-define
+    if (!body.classList.contains(OPENED_MENU_CLASSNAME)) {
+      // eslint-disable-next-line no-use-before-define
+      body.classList.add(OPENED_MENU_CLASSNAME);
+    } else {
+      // eslint-disable-next-line no-use-before-define
+      body.classList.remove(OPENED_MENU_CLASSNAME);
+    }
+  };
+  const OPENED_MENU_CLASSNAME = 'main-menu-opened';
 });
